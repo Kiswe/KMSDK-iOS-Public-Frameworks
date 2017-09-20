b5f93b931ead24c82d40406397ba9b77fea58c05
Chris Boraski <chris.boraski@kiswe.com>
2017-09-20 10:40:59 -0400

    Updates to fix XCode 9 / iOS 11 issues

---
+++ KMSDK.framework/Headers/KMSDK-Private-umbrella.h
@@ -0,0 +1,30 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "KMAPIClient.h"
+#import "HMSegmentedControl.h"
+#import "CircleView.h"
+#import "DDLogSettings.h"
+#import "KMCMovableImage.h"
+#import "KMParentViewController.h"
+#import "KMStatusLabel.h"
+#import "NascarCarView.h"
+#import "NascarTrackCharlotteMotorSpeedway.h"
+#import "NascarTrackHelper.h"
+#import "NascarTrackView.h"
+#import "RectView.h"
+#import "TKChatViewController.h"
+#import "TKMediaPlayerObserver.h"
+
+FOUNDATION_EXPORT double KMSDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char KMSDKVersionString[];
+
